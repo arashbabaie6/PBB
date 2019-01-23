d8e4eb733e7d1d2c0e16a91e7d0e6632e2ce9b90
arashbabaie6 <arash.babaie6@gmail.com>
2019-01-23 16:27:23 +0330

    main leauge leader dynamic

---
+++ routes/mainLeaugeLeaders.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+function modifyLeaugeLeaders(obj) {
+  return obj.resultSet.find(r => r.name == "LeadersTiles");
+}
+
+router.get("/:statCategory/Player", (req, res) => {
+  axios
+    .get(
+      `http://stats.nba.com/stats/leaderstiles/?leagueId=00&season=2018-19&seasonType=Regular+Season&stat=${
+        req.params.statCategory
+      }&playerOrTeam=Player&gameScope=Season&playerScope=All+Players`
+    )
+    .then(response => {
+      let responseClient = modifyLeaugeLeaders(response.data);
+      res.json(responseClient);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+router.get("/:statCategory/Team", (req, res) => {
+    axios
+      .get(
+        `http://stats.nba.com/stats/leaderstiles/?leagueId=00&season=2018-19&seasonType=Regular+Season&stat=${
+          req.params.statCategory
+        }&playerOrTeam=Team&gameScope=Season&playerScope=All+Players`
+      )
+      .then(response => {
+        let responseClient = modifyLeaugeLeaders(response.data);
+        res.json(responseClient);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,26 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors')
-const config = require('./config');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const standing = require('./routes/standing');
-const scoreboard = require('./routes/scoreboard')
+const cors = require("cors");
+const config = require("./config");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const standing = require("./routes/standing");
+const scoreboard = require("./routes/scoreboard");
+const mainLeaugeLeaders = require("./routes/mainLeaugeLeaders");
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/standing', standing);
-app.use('/scoreboard',scoreboard )
-
-app.listen(config.PORT,()=>{
-    mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true});
-    console.log(`server is listening on port ${config.PORT} !`)
+app.use("/standing", standing);
+app.use("/scoreboard", scoreboard);
+app.use("/mainLeaugeLeaders", mainLeaugeLeaders);
+app.listen(config.PORT, () => {
+  mongoose.connect(
+    config.MONGODB_URI,
+    { useNewUrlParser: true }
+  );
+  console.log(`server is listening on port ${config.PORT} !`);
 });
 
 const db = mongoose.connection;
-db.on('error', err => console.log(err));
+db.on("error", err => console.log(err));
 
-db.once('open', () => {
-    console.log(`Database is connected!`);
-  });
+db.once("open", () => {
+  console.log(`Database is connected!`);
+});
